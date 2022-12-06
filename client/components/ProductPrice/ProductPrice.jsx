40bfaf1729f5b27ada2a74037102a54fad157805
Gabi Cortes <g-cortes@hotmail.es>
2022-12-06 22:49:53 +0000

    basic set up for product section

--- client/components/ProductPrice/ProductPrice.jsx
+++ client/components/ProductPrice/ProductPrice.jsx
@@ -4,7 +4,23 @@ import styles from "./ProductPrice.module.css";
 export function ProductPrice() {
   return (
     <section className={styles.section}>
-      <img src={bulb.src} alt="product image" />
+      <img className={styles.productImg} src={bulb.src} alt="product image" />
+      <h1 className={styles.h2}>Energy saving light bulb</h1>
+      <span>25 W // Packet of 4</span>
+      <div className={styles.priceWrapper}>
+        <span>£12.99</span>
+        <div className={styles.productQuantityWrapper}>
+          <span>Qty</span>
+          <div className={styles.buttonsWrapper}>
+            <button className={styles.buttonDecrease}>-</button>
+            <div className={styles.quantityProduct}>1</div>
+            <button className={styles.buttonIncrease}>+</button>
+          </div>
+        </div>
+      </div>
+      <button className={styles.buttonAddToCart} type="submit">
+        Add to cart
+      </button>
     </section>
   );
 }
