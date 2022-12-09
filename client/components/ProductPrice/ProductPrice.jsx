238bcda6327445018a5289c20660c9cd8c1ae421
Gabi Cortes <g-cortes@hotmail.es>
2022-12-09 00:19:04 +0000

    responsive design

--- client/components/ProductPrice/ProductPrice.jsx
+++ client/components/ProductPrice/ProductPrice.jsx
@@ -19,47 +19,49 @@ export function ProductPrice(props) {
         src={product.img_url}
         alt="product image"
       />
-      <h1 className={styles.productName}>{product.name}</h1>
-      <span className={styles.detailQuantity}>
-        {product.power} // Packet of {product.quantity}
-      </span>
-      <div className={styles.priceWrapper}>
-        <div className={styles.price}>
-          £{((product.price / 100) * count).toFixed(2)}
-        </div>
-        <div className={styles.productQuantityWrapper}>
-          <span className={styles.quantityProduct}>Qty</span>
-          <div className={styles.buttonsWrapper}>
-            <button
-              className={styles.buttonDecrease}
-              type="button"
-              onClick={() => {
-                setCount(count--);
-              }}
-              disabled={count < 2}
-            >
-              -
-            </button>
-            <div className={styles.quantityCount}>{count}</div>
-            <button
-              className={styles.buttonIncrease}
-              type="button"
-              onClick={() => {
-                setCount(count++);
-              }}
-            >
-              +
-            </button>
+      <div className={styles.priceDetailsWrapper}>
+        <h1 className={styles.productName}>{product.name}</h1>
+        <span className={styles.detailQuantity}>
+          {product.power} // Packet of {product.quantity}
+        </span>
+        <div className={styles.priceWrapper}>
+          <div className={styles.price}>
+            £{((product.price / 100) * count).toFixed(2)}
+          </div>
+          <div className={styles.productQuantityWrapper}>
+            <span className={styles.quantityProduct}>Qty</span>
+            <div className={styles.buttonsWrapper}>
+              <button
+                className={styles.buttonDecrease}
+                type="button"
+                onClick={() => {
+                  setCount(count--);
+                }}
+                disabled={count < 2}
+              >
+                -
+              </button>
+              <div className={styles.quantityCount}>{count}</div>
+              <button
+                className={styles.buttonIncrease}
+                type="button"
+                onClick={() => {
+                  setCount(count++);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
+        <button
+          className={styles.buttonAddToCart}
+          type="submit"
+          onClick={handleClickAddToCart}
+        >
+          Add to cart
+        </button>
       </div>
-      <button
-        className={styles.buttonAddToCart}
-        type="submit"
-        onClick={handleClickAddToCart}
-      >
-        Add to cart
-      </button>
     </section>
   );
 }
